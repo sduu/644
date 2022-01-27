e3b1479c0987ca5b9c4245c4edbeca75c40de130
Hyesu Son <46313348+sduu@users.noreply.github.com>
2022-01-27 11:27:18 +0900

    Refactor : 캔버스 스크립트 리팩토링
    캔버스 배경 인터렉션을 재생, 일시정지 할 수 있도록 확장

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-(() => {
-	/* init */
-	initCanvas();
+const canvas = document.querySelector('#stage');
+const canvasWrap = canvas.parentNode || canvas;
+const context = canvas.getContext('2d');
 
-	/* function */
-	function initCanvas() {
-		const canvas = document.querySelector('#stage');
-		const canvasWrap = canvas.parentNode || canvas;
-		const context = canvas.getContext('2d');
+let resizeTimer;
 
+const background = (function() {
+	const rows = 6;
+	const itemArray = [];
+	let itemWidth = Math.min(canvas.width / 10, 150);
+
+	let isAnimate = false;
+	
+	var init = function(){
 		canvas.width = canvasWrap.offsetWidth;
 		canvas.height = canvasWrap.offsetHeight;
 
@@ -16,32 +20,11 @@
 		canvas.offCanvas.width = 512;
 		canvas.offCanvas.height = 512;
 
-		const rows = 6;
-		const cakeArray = [];
+		setItem();
+		resize();
+	};
 
-		let resizeTimer;
-		let cakeWidth = Math.min(canvas.width / 10, 150);
-
-		class Cake {
-			constructor(x, y, rows) {
-				this.rows = rows;
-				this.x = x;
-				this.y = canvas.height / 4 * y;
-				this.draw();
-			}
-
-			draw() {
-				if (this.y < -cakeWidth) {
-					this.y = canvas.height / 4 * rows - cakeWidth;
-				}
-				if (this.rows === 'odd') {
-					context.drawImage(canvas.offCanvas, canvas.width / 5 * this.x + (canvas.width / 10 - cakeWidth / 2), this.y, cakeWidth, cakeWidth);
-				} else {
-					context.drawImage(canvas.offCanvas, canvas.width / 5 * this.x + canvas.width / 5 + (canvas.width / 10 - cakeWidth / 2), this.y, cakeWidth, cakeWidth);
-				}
-			}
-		}
-
+	var setItem = function() {
 		const imgElem = new Image();
 		imgElem.src = './images/img-birthday-cake.png';
 		imgElem.addEventListener('load', () => {
@@ -53,56 +36,100 @@
 					for (let j = 0; j < 3; j++) {
 						x = j * 2;
 						y = (i - 1);
-						cakeArray.push(new Cake(x, y, 'odd'));
+						itemArray.push(new Item(x, y, 'odd'));
 					}
 				} else {
 					for (let j = 0; j < 2; j++) {
 						x = j * 2;
 						y = (i - 1);
-						cakeArray.push(new Cake(x, y, 'even'));
+						itemArray.push(new Item(x, y, 'even'));
 					}
 				}
 			}
-			update();
 		});
+	};
 
-		window.addEventListener('resize', function() {
-			clearTimeout(resizeTimer);
-			resizeTimer = setTimeout(function() {
-				resize();
-			}, 0.5);
-		});
+	var drawBoard = function (){
+		for (let x = 0; x <= canvas.width; x += canvas.width / 5) {
+			context.moveTo(0.5 + x, 0);
+			context.lineTo(0.5 + x, canvas.height);
+		}
+		for (let x = 0; x <= canvas.height; x += canvas.height / 4) {
+			context.moveTo(0, 0.5 + x);
+			context.lineTo(canvas.width, 0.5 + x);
+		}
+		context.strokeStyle = "black";
+		context.stroke();
+	};
 
-		function drawBoard(){
-			for (var x = 0; x <= canvas.width; x += canvas.width / 5) {
-				context.moveTo(0.5 + x, 0);
-				context.lineTo(0.5 + x, canvas.height);
-			}
-			for (var x = 0; x <= canvas.height; x += canvas.height / 4) {
-				context.moveTo(0, 0.5 + x);
-				context.lineTo(canvas.width, 0.5 + x);
-			}
-			context.strokeStyle = "black";
-			context.stroke();
+	var update = function() {
+		if (!isAnimate) {
+			return false;
 		}
 
-		function update() {
-			context.clearRect(0, 0, canvas.width, canvas.height);
-			drawBoard();
-			let cake;
-			for (let i = 0; i < cakeArray.length; i++) {
-				cake = cakeArray[i];
-				cake.y -= 0.5;
-				cake.draw();
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		//drawBoard();
+		let item;
+		for (let i = 0; i < itemArray.length; i++) {
+			item = itemArray[i];
+			item.y -= 0.5;
+			item.draw();
+		}
+
+		requestAnimationFrame(update);
+	};
+
+	var playAnimation = function() {
+		isAnimate = true;
+		update();
+	};
+
+	var pauseAnimation = function() {
+		isAnimate = false;
+	};
+
+	var resize = function() {
+		canvas.width = canvasWrap.offsetWidth;
+		canvas.height = canvasWrap.offsetHeight;
+		itemWidth = Math.min(canvas.width / 10, 150);
+	};
+
+	class Item {
+		constructor(x, y, line) {
+			this.line = line;
+			this.x = x;
+			this.y = canvas.height / 4 * y;
+			this.draw();
+		}
+
+		draw() {
+			if (this.y < -itemWidth) {
+				this.y = canvas.height / 4 * line - itemWidth;
 			}
-
-			requestAnimationFrame(update);
+			if (this.line === 'odd') {
+				context.drawImage(canvas.offCanvas, canvas.width / 5 * this.x + (canvas.width / 10 - itemWidth / 2), this.y, itemWidth, itemWidth);
+			} else {
+				context.drawImage(canvas.offCanvas, canvas.width / 5 * this.x + canvas.width / 5 + (canvas.width / 10 - itemWidth / 2), this.y, itemWidth, itemWidth);
+			}
 		}
+	};
 
-		function resize() {
-			canvas.width = canvasWrap.offsetWidth;
-			canvas.height = canvasWrap.offsetHeight;
-			cakeWidth = Math.min(canvas.width / 10, 150);
-		}
-	}
+	return {
+		init: init,
+		resize: resize,
+		play: playAnimation,
+		pause: pauseAnimation
+	};
 })();
+
+/* init */
+background.init();
+background.play();
+
+/* add event */
+window.addEventListener('resize', function() {
+	clearTimeout(resizeTimer);
+	resizeTimer = setTimeout(function() {
+		background.resize();
+	}, 0.5);
+});
